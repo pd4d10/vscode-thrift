495fa742b719d0248f4339283c9e32f8d36214f5
Rongjian Zhang <pd4d10@gmail.com>
2018-09-08 23:56:45 +0800

    style: prettier format

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-'use strict';
+'use strict'
 
 import * as vscode from 'vscode'
 import * as assert from 'assert'
@@ -13,9 +13,24 @@ describe('Should get diagnostics', () => {
 
   it('Diagnoses uppercase texts', async () => {
     await testDiagnostics(docUri, [
-      { message: 'ANY is all uppercase.', range: toRange(0, 0, 0, 3), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-      { message: 'ANY is all uppercase.', range: toRange(0, 14, 0, 17), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-      { message: 'OS is all uppercase.', range: toRange(0, 18, 0, 20), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' }
+      {
+        message: 'ANY is all uppercase.',
+        range: toRange(0, 0, 0, 3),
+        severity: vscode.DiagnosticSeverity.Warning,
+        source: 'ex',
+      },
+      {
+        message: 'ANY is all uppercase.',
+        range: toRange(0, 14, 0, 17),
+        severity: vscode.DiagnosticSeverity.Warning,
+        source: 'ex',
+      },
+      {
+        message: 'OS is all uppercase.',
+        range: toRange(0, 18, 0, 20),
+        severity: vscode.DiagnosticSeverity.Warning,
+        source: 'ex',
+      },
     ])
   })
 })
@@ -26,12 +41,15 @@ function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
   return new vscode.Range(start, end)
 }
 
-async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
+async function testDiagnostics(
+  docUri: vscode.Uri,
+  expectedDiagnostics: vscode.Diagnostic[],
+) {
   await activate(docUri)
 
-  const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+  const actualDiagnostics = vscode.languages.getDiagnostics(docUri)
 
-  assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+  assert.equal(actualDiagnostics.length, expectedDiagnostics.length)
 
   expectedDiagnostics.forEach((expectedDiagnostic, i) => {
     const actualDiagnostic = actualDiagnostics[i]
@@ -39,4 +57,4 @@ async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.D
     assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range)
     assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity)
   })
-} 
+}
